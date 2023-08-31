3942c175240ab3b2ec4f4e1eaa5c166493ea680a
Michele Rizzello <mrizzello@gmail.com>
2023-08-31 15:49:34 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import periodData from '../assets/period.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'periode';
+export class AppComponent implements OnDestroy {
+  title = 'période';
+  periodData = periodData;
+  currentPeriod: any;
+  currentTime: Date = new Date();
+  remainingMinutes!: number;
+  remainingSeconds!: number;
+  progress!: number;
+  interval: any;
+
+  ngOnInit() {
+    this.updateValues();
+    this.interval = setInterval(() => {
+      this.updateValues();
+    }, 1000);
+  }
+
+  updateValues() {
+    this.currentTime = new Date();
+    this.setPeriod();
+    if (this.currentPeriod) {
+      const startTime = this.parseTime(this.currentPeriod.start);
+      const endTime = this.parseTime(this.currentPeriod.end);
+      const timeDifference = endTime.getTime() - this.currentTime.getTime();
+
+      const timeRemainingInSeconds = Math.max(0, Math.floor(timeDifference / 1000));
+      this.remainingMinutes = Math.floor(timeRemainingInSeconds / 60);
+      this.remainingSeconds = timeRemainingInSeconds % 60;
+
+      const totalTime = endTime.getTime() - startTime.getTime();
+      const elapsedTime = this.currentTime.getTime() - startTime.getTime();
+      this.progress = (elapsedTime / totalTime) * 100;
+    }
+  }
+
+  parseTime(timeString: string): Date {
+    const [hours, minutes] = timeString.split(':').map(Number);
+    const time = new Date();
+    time.setHours(hours, minutes, 0);
+    return time;
+  }
+
+  setPeriod() {
+    const currentTime = new Date();
+    const currentDayOfWeek = currentTime.getDay();
+
+    if (currentDayOfWeek === 0 || currentDayOfWeek === 6) {
+      this.currentPeriod = null;
+    }
+    
+    for (let i = 0; i < this.periodData.length; i++) {
+      const currentPeriod = this.periodData[i];
+      const nextPeriod = this.periodData[i + 1];
+
+      const startTime = this.parseTime(currentPeriod.start);
+      const endTime = this.parseTime(currentPeriod.end);
+      
+      if (startTime <= currentTime && currentTime <= endTime) {
+        this.currentPeriod = currentPeriod;
+        return;
+      }
+      
+      if (nextPeriod && currentTime >= endTime && currentTime <= this.parseTime(nextPeriod.start)) {
+        this.currentPeriod = {
+          period: 'pause',
+          start: endTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}),
+          end: this.parseTime(nextPeriod.start).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+        };
+        return;
+      }
+    }
+    this.currentPeriod = null;
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatProgressBarModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
